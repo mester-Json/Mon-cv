f4fb74b9c27c5e4e3e68e2159fd9d01c677147ac
root <root@asusjayson.home>
2024-04-11 12:16:41 +0200

    Maj du style

--- accueil-function.js
+++ accueil-function.js
@@ -1,28 +1,21 @@
-import emailjs from 'emailjs-com';
 
-// Initialisation de Email.js avec vos identifiants API
 emailjs.init('SGbl2DwG5zNA_zMMo');
 
-// Gestion de la soumission du formulaire
 document.getElementById('emailForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Obtention des valeurs des champs
     const to = document.getElementById('to').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
 
-    // Envoi de l'e-mail
     emailjs.send('service_qjlaqoh', 'template_sb0zwts', {
         to_email: to,
         subject: subject,
         message: message
     }).then(function (response) {
         console.log('E-mail envoyé avec succès!', response);
-        // Vous pouvez ici ajouter du code pour afficher un message de confirmation
     }, function (error) {
         console.log('Erreur lors de l\'envoi de l\'e-mail', error)
-        // Vous pouvez ici ajouter du code pour afficher un message d'erreur
     });
 });
 
@@ -32,30 +25,22 @@ const closeModalBtn = document.getElementById('closeModal');
 const emailForm = document.getElementById('emailForm');
 const sendButton = emailForm.querySelector('input[type="submit"]');
 
-// Ouvrir la modal lorsque le lien est cliqué
 openModalLink.addEventListener('click', () => {
     modal.style.display = 'block';
 });
 
-// Fermer la modal lorsque le bouton "Fermer" est cliqué
 closeModalBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-// Fermer la modal si l'utilisateur clique en dehors de la modal
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
 });
 
-// Ajouter un gestionnaire d'événement de soumission du formulaire
 emailForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Ici, vous pouvez ajouter le code nécessaire pour envoyer l'e-mail.
-    // Après l'envoi réussi de l'e-mail, vous pouvez fermer la modal
-
-    // Fermer la modal
     modal.style.display = 'none';
 });
